76e76fcdeac9168c87a1ba2a3bf2094c4d1154b4
FMotalleb <m.falahnejad@dornica.net>
2025-03-11 11:11:40 +0330

    feat: Implement unShadow function to rename conflicting page variable in template context

--- engine/template.go
+++ engine/template.go
@@ -3,17 +3,31 @@ package engine
 import (
 	"bytes"
 	"fmt"
+	"log/slog"
 	"text/template"
 
 	"github.com/playwright-community/playwright-go"
 )
 
+func unShadow(data map[string]any, key string) map[string]any {
+	if _, exists := data[key]; exists {
+		newKey := fmt.Sprintf("_%s", key)
+		data = unShadow(data, newKey)
+		data[newKey] = data[key]
+	} else {
+	}
+	return data
+}
 func applyTemplate(text string, vars Vars, page playwright.Page) (string, error) {
 	tmpl, err := template.New("template").Parse(text)
 	if err != nil {
 		return "", fmt.Errorf("failed to parse template: %s", err)
 	}
 	variables := vars.LiveSnapshot()
+	if _, ok := variables["page"]; ok {
+		slog.Error("found a page variable in live snapshot generated for template, renaming old page variable to _page")
+		variables = unShadow(variables, "page")
+	}
 	variables["page"] = page
 	output := bytes.NewBufferString("")
 	err = tmpl.Execute(output, variables)
