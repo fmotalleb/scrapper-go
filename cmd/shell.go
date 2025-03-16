da0cd78d5676c40d66673605da6084dfe1dcf4a8
FMotalleb <m.falahnejad@dornica.net>
2025-03-16 08:42:13 +0330

    feat: Add interactive shell command for scrapper

---
+++ cmd/shell.go
@@ -0,0 +1,22 @@
+/*
+Copyright © 2025 NAME HERE <EMAIL ADDRESS>
+*/
+package cmd
+
+import (
+	"github.com/fmotalleb/scrapper-go/shell"
+	"github.com/spf13/cobra"
+)
+
+// shellCmd represents the shell command
+var shellCmd = &cobra.Command{
+	Use:   "shell",
+	Short: "Start scrapper in an interactive shell",
+	Run: func(cmd *cobra.Command, args []string) {
+		shell.RunShell()
+	},
+}
+
+func init() {
+	rootCmd.AddCommand(shellCmd)
+}
