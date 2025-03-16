531349997df052406d3a7f3011b772ca07726bd0
FMotalleb <m.falahnejad@dornica.net>
2025-03-16 16:49:48 +0330

    docs: log_reader

--- shell/log_reader.go
+++ shell/log_reader.go
@@ -7,30 +7,36 @@ import (
 	"github.com/rivo/tview"
 )
 
+// createLogView initializes the log view for displaying logs in the UI.
 func createLogView() *tview.TextView {
 	logBox := tview.NewTextView()
-	logBox.SetText("waiting for logs to arrive ...")
-	logBox.SetTextStyle(outputTheme)
-	logBox.SetBackgroundColor(backgroundColor).
-		SetBorder(true)
-	logBox.SetBorderStyle(outputTheme)
+	logBox.SetText("waiting for logs to arrive ...").
+		SetTextStyle(outputTheme).
+		SetBackgroundColor(backgroundColor).
+		SetBorder(true).
+		SetBorderStyle(outputTheme)
+
 	logOutput := &logBuffer{
 		viewPort: logBox,
 		buffer:   "",
 	}
+
+	// Setup the logger to write to the log view.
 	log.SetupLogger("DEBUG", logOutput)
+
 	return logBox
 }
 
+// logBuffer is a custom type that implements io.Writer to capture log output.
 type logBuffer struct {
 	viewPort *tview.TextView
 	buffer   string
 }
 
-// Write implements io.Writer.
+// Write implements the io.Writer interface for logBuffer.
 func (l *logBuffer) Write(p []byte) (n int, err error) {
 	l.buffer = fmt.Sprintf("%s%s", l.buffer, string(p))
-	l.viewPort.SetText(l.buffer)
-	l.viewPort.ScrollToEnd()
+	l.viewPort.SetText(l.buffer) // Update the log view with the current buffer.
+	l.viewPort.ScrollToEnd()     // Scroll to the end of the log view.
 	return len(p), nil
 }
