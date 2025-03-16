58eccc32ec656364800d5f531e5708bcb52558f5
FMotalleb <m.falahnejad@dornica.net>
2025-03-16 16:50:35 +0330

    docs: output

--- shell/output.go
+++ shell/output.go
@@ -2,15 +2,21 @@ package shell
 
 import "github.com/rivo/tview"
 
+// buildResultView creates and returns a TreeView to display the data from the provided channel.
 func buildResultView(dataLink <-chan map[string]any) tview.Primitive {
 	output := tview.NewTreeView()
+
+	// Goroutine to listen for data from the channel and update the TreeView.
 	go func() {
-		for i := range dataLink {
-			output.SetRoot(buildTree(i))
+		for data := range dataLink {
+			output.SetRoot(buildTree(data)) // Update the root of the TreeView with the new data.
 		}
 	}()
-	output.SetBackgroundColor(outputBg)
-	output.SetBorder(true).SetBorderStyle(outputTheme)
+
+	// Set the background color and border style for the TreeView.
+	output.SetBackgroundColor(outputBg).
+		SetBorder(true).
+		SetBorderStyle(outputTheme)
 
 	return output
 }
